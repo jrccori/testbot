c93203176c0d50420c9d33d1ff135019971765de
jrccori <jrsevenj@gmail.com>
2025-04-21 14:33:29 -0500

    newchanges

--- app.py
+++ app.py
@@ -13,6 +13,3 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return '¡Hola desde Flask en Railway! 🚂'
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
