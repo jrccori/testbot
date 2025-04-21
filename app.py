5d9507ca2e2f0e6e1996535f8a54a295bbc80b69
josue ccorisoncco <38757794+jrccori@users.noreply.github.com>
2025-04-21 12:41:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '¡Hola desde Flask en Netlify! 🎉'
+    return '¡Hola desde Flask en Railway! 🚂'
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # Usa 5000 si no se encuentra PORT
+    app.run(host='0.0.0.0', port=port)
