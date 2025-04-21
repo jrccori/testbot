72c3dbca0b0b9ef11339a257c0c253985605b60d
jrccori <jrsevenj@gmail.com>
2025-04-21 14:28:48 -0500

    new app.py

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
+from flask import Flask,session, render_template, request, jsonify,url_for,redirect
+import whisper
+import tempfile
 import os
-from flask import Flask
-
+from transformers import BlenderbotTokenizer, BlenderbotForConditionalGeneration
+from transformers import pipeline, AutoTokenizer, AutoModelForSeq2SeqLM
+from transformers import MarianMTModel, MarianTokenizer
+import requests
+import random
+import os
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return '¡Hola desde Flask en Railway! 🚂'
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
