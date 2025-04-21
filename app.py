251637da8d0cf2d855d7af584a3ce727ad672b69
josue ccorisoncco <38757794+jrccori@users.noreply.github.com>
2025-04-21 12:49:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,3 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return '¡Hola desde Flask en Railway! 🚂'
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Usa 5000 si no se encuentra PORT
-    app.run(host='0.0.0.0', port=port)
